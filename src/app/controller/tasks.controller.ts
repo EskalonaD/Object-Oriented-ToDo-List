2f8ac285f15a4725c5baa24e1fbeba8eb583d265
EskalonaD <mrragday@gmail.com>
2021-03-11 12:38:47 -0800

    define tasks controller interface

--- src/app/controller/tasks.controller.ts
+++ src/app/controller/tasks.controller.ts
@@ -1,2 +1,16 @@
+import { unexposedTask, Task } from '../model-layer';
+import { extraData } from '../model-layer/task/task';
+import { TasksModel, updatedTaskData } from '../model-layer/tasks.model';
+
 export class TasksController {
+    constructor (private tasksModel: TasksModel) {}
+
+    addTask(task: updatedTaskData) {}
+    updateTask(Task: unexposedTask, taskData: updatedTaskData) {}
+    deleteTask(task: unexposedTask) {}
+    addField(task: unexposedTask, taskData: extraData) {}
+    deleteField(task: unexposedTask, fieldName: string) {}
+    completeTask(task: unexposedTask) {}
+    changeDescription(task: unexposedTask, description: string) {}
+
 }
