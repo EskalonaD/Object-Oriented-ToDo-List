3e13600af1c4a4d4bc5d8e3eeeb90b814a85fe78
EskalonaD <mrragday@gmail.com>
2021-03-11 12:33:56 -0800

    created testing data fetch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Initiazer } from './temp/initiazer';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER, useValue: (new Initiazer).init, multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
